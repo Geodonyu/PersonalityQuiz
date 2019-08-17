9de456a209427699fe733f79298643bdb90b9c95
Haorong Qiu <haorong.qiu@gmail.com>
2019-08-17 10:46:36 -0400

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Haorong Qiu on 8/17/19.
+//  Copyright © 2019 Haorong Qiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
