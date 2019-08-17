6b87ad77189793c355508a3644d3fdf28cc4a4d9
Haorong Qiu <haorong.qiu@gmail.com>
2019-08-17 16:16:08 -0400

    Created Personality Quiz Project

---
+++ PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Haorong Qiu on 8/17/19.
+//  Copyright © 2019 Haorong Qiu. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
